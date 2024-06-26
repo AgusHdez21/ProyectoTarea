ba7e9b8b02119545fe2b0b38932b2bfce5707e26
AgusHdez21 <20200700@uthh.edu.mx>
2024-06-26 02:59:24 -0600

    cscs

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 import logging
 
 app = Flask(__name__)
 
-def create_model_RFE():
-    model = Sequential()
-    model.add(Dense(64, input_dim=10, activation='relu'))  # Ajusta input_dim según tus datos
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(1, activation='linear'))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    return model
-
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
 # Cargar el modelo entrenado y el escalador
 model = joblib.load('modeloNeuR2.pkl')
 scaler = joblib.load('DataScaled.pkl')
@@ -31,43 +22,24 @@ def predict():
         year = float(request.form['year'])
         driven = float(request.form['km_driven'])
         engine = request.form['Engine (CC)'] 
-        max_power = float(request.form['max_power'])
-
-        # Verificar los datos recibidos
-        app.logger.debug(f'year: {year}, km_driven: {driven}, Engine (CC): {engine}, max_power (in bph): {max_power}')
+        max_power = float(request.form['max_power (in bph)'])
 
         # Crear el DataFrame de entrada
         input_data = pd.DataFrame({
-            'Unnamed: 0': [0],
-            'name': [0],
             'year': [year],
             'km_driven': [driven],
-            'fuel': [0],
-            'seller_type': [0],
-            'owner': [0],
-            'seats': [0],
             'max_power (in bph)': [max_power],
-            'Mileage': [0], 
-            'Engine (CC)': [engine],
-            'Mileage Unit_km/kg': [0],
-            'Mileage Unit_kmpl': [0],
-            'transmission_Automatic': [0],
-            'transmission_Manual': [0],
+            'Engine (CC)': [engine]
         })
 
         # Escalar los datos de entrada
         scaled_data = scaler.transform(input_data)
 
-        # Seleccionar solo las características usadas para el modelo
-        # Asegúrate de que los índices sean correctos según tu modelo
-        scaled_data_for_prediction = scaled_data[:, [2, 3, 8, 10]]
-
         # Realizar la predicción con los datos escalados
-        prediccion = model.predict(scaled_data_for_prediction)
+        prediction = model.predict(scaled_data)
+        prediction_value = round(float(prediction[0]), 2)
 
         # Devolver la predicción como JSON
-        prediction_value = round(float(prediccion[0]), 2)
-
         return jsonify({'prediction': prediction_value})
 
     except Exception as e:
